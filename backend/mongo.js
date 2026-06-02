fe121f8d6616440d15cb78f299f812b4304e045e
ladariusglover <ladariusglover03@gmail.com>
2026-06-02 19:58:18 -0400

    frontend and backend folder push to github

---
+++ backend/mongo.js
@@ -0,0 +1,14 @@
+const {MongoClient} = require('mongodb');
+let db;
+async function connectMongo() {
+    const client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
+    db = client.db('ecommerce_db');
+    console.log('Mongodb connected');
+}
+
+function getMongo() {
+    return db;
+}
+
+module.exports = {connectMongo, getMongo};
---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+const {connectMongo} = require('./mongo');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message:'ERA Commerece API is running.'});
+});
+async function startServer(){
+    await connectMongo();
+    app.listen(PORT, () => {
+        console.log(`server running at http://localhost:${PORT}`);
+    });
+}
+startServer();
